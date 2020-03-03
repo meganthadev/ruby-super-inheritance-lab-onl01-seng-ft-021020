6d6e7fa7077757dc4f38aa6aeb4f70a071c7f999
Megan Horsfield <meganthadev@gmail.com>
2020-03-03 00:16:54 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,14 @@
 class ChattyStudent < Student 
   
-
+def hello
+  super
+    puts "Hey there! I'm so excited to learn stuff."
+  end  
+  
+  def raise_hand
+    super
+    puts "Pick me!"
+  end 
   
   
   
